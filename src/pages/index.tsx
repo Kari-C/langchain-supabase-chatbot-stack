ef4e9df605b776e1bb38f21042852e17c6895962
Kari-C <kc@jenjetson.com>
2024-04-27 22:37:24 -0500

    Chatscope chat style overrides
    Must do all this to over-ride the default styles.
    https://github.com/chatscope/chat-ui-kit-styles/issues/5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -139,8 +139,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main >
         <div
+          className="bg-white dark:bg-slate-800 dark:text-white"
           style={{ position: "relative", height: "98vh", overflow: "hidden" }}
         >
           <MainContainer>
