76ea826ff0a8a249a054bfa1a638250e221b4a14
Kari-C <kc@jenjetson.com>
2024-04-27 22:34:59 -0500

    Add tailwind, needs follow up
    Double check src/styles/input.css and output.css not doing anything
    npx tailwindcss -i ./src/styles/input.css -o ./public/output.css --watch
    tailwind.config.js needs theme adjustments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@
 
 import { AppProps } from 'next/app'
 import '../styles/main.scss'
+import '../styles/output.css'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
